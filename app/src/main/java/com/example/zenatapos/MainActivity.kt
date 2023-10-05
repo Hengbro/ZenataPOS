0a8bb922cc1c201efc60543ea2e8bba07132a833
Hengbro <hengkisaoremaha@gmail.com>
2023-10-06 02:10:03 +0700

    init message

--- app/src/main/java/com/example/zenatapos/MainActivity.kt
+++ app/src/main/java/com/example/zenatapos/MainActivity.kt
@@ -8,18 +8,19 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.zenatapos.databinding.ActivityMainBinding
+import com.example.zenatapos.databinding.HomecheckoutBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: HomecheckoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = HomecheckoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        /*val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
@@ -30,6 +31,6 @@ class MainActivity : AppCompatActivity() {
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        navView.setupWithNavController(navController)*/
     }
 }
--- app/src/main/java/com/example/zenatapos/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/zenatapos/ui/auth/LoginActivity.kt
@@ -3,15 +3,16 @@ package com.example.zenatapos.ui.auth
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.zenatapos.databinding.ActivityLoginBinding
+import com.example.zenatapos.databinding.HomecheckoutBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding: HomecheckoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = HomecheckoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
